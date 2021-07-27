968296b98546c6ccbcaf143738e85b72e8918ed4
mkotte <74936870+mkotte@users.noreply.github.com>
2021-07-27 18:42:32 -0400

    updated work section, added modals. Looking good!

--- scripts/js/works.js
+++ scripts/js/works.js
@@ -4,7 +4,7 @@ const projectInfo = [
         name: "Custom Workforce Manager",
         repo:"https://github.com/mkotte/Custom-Workforce-Manager",
         app:"https://custom-workforce-manager.herokuapp.com/",
-        description:"An application that was created to streamline communication between administration and its workforce while maintaining an organized log of the company's job information.",
+        description:"An application that was created to streamline communication between administration and its workforce while maintaining an organized log of the company's job information. This application can take a little while to load, due to hosting with Heroku.",
         tech:["MongoDb","Express","React","Node", "Material-UI", "Passport.js", "SortableJS", "Heroku"],
         iconClass: "fas fa-user-tie"
     },
@@ -12,7 +12,7 @@ const projectInfo = [
         name: "COVID Woke",
         repo:"https://github.com/mkotte/COVIDwoke",
         app:"https://mkotte.github.io/COVIDwoke/",
-        description:"COVID woke seeks to elevate the public information environment by presenting necessary information that is accessible and easy to understand.",
+        description:"A website that seeks to elevate the public information environment by presenting necessary information that is accessible and easy to understand.",
         tech:["Javascript", "HTML", "CSS", "Bootstrap", "3rd-Party API", "Chart.js"],
         iconClass: "fas fa-virus"
     },
@@ -80,6 +80,7 @@ const projectNames = (j) => {
     let projectNameWrapper = document.createElement("div")
     projectNameWrapper.setAttribute("class", "projectNameWrapper");
     projectBox.appendChild(projectNameWrapper)
+
     let projectName = document.createElement("h3")
     projectName.setAttribute("class", "projectName");
     projectName.textContent = projectInfo[j].name
@@ -96,7 +97,17 @@ for (i=0; i < projectInfo.length; i++){
 
 // appending technology used to modal
 const addTechUsed = (index) => {
+    // retrieving relative project information
     const techArray = projectInfo[index].tech
+
+    // adding a header for clarification
+    const techWrapper = document.querySelector(".technologyWrapper")
+    const techHeader = document.createElement('h4');
+    techHeader.setAttribute("class", "techHeader");
+    techHeader.textContent = "Technology Used:"
+    techWrapper.insertBefore(techHeader, techWrapper.firstChild);
+    
+    // iterating through the list items
     for (i=0; i < techArray.length; i++){
         const list = document.querySelector(".techList")
         const tech = document.createElement('li')
@@ -105,15 +116,38 @@ const addTechUsed = (index) => {
     }
 }  
 
+const addButtonFunctionality = (index) => {
+    let footer = document.querySelector(".modal-footer")
+
+    if (projectInfo[index].app !== 'none'){
+        // adding application button and functionality
+        let appBtn = document.createElement("button")
+        appBtn.setAttribute("class", "btn")
+        appBtn.setAttribute("type", "button")
+        appBtn.textContent = "Application"
+        footer.appendChild(appBtn)
+        appBtn.addEventListener('click', function (event) {
+            location.href = projectInfo[index].app;
+        });
+    }
+
+    // adding repository button and functionality
+    let repoBtn = document.createElement("button")
+    repoBtn.setAttribute("class", "btn")
+    repoBtn.setAttribute("type", "button")
+    repoBtn.textContent = "Repository"
+    footer.appendChild(repoBtn)
+    repoBtn.addEventListener('click', function (event) {
+        location.href = projectInfo[index].repo;
+    });
+}
+
 // function for removing all child nodes
 const removeAllChildNodes = (parent) => {
     while (parent.firstChild){
         parent.removeChild(parent.firstChild);
     }
 }
-
-
-
 
 // generic event listener on projectBox's modals
 // using bootstraps method of varying modal content
@@ -124,11 +158,20 @@ $('#projectModal').on('show.bs.modal', function (event) {
     modal.find('.modal-title').text(projectInfo[info].name)
     modal.find('.modal-description').text(projectInfo[info].description)
     addTechUsed(info);
+    addButtonFunctionality(info);
 });
 
 // removing appended tech on "closing" of modal
 $('#projectModal').on('hidden.bs.modal', function () {
-    // const modalCloseBtn = document.querySelector('#closeModal')
+    // removing added list items
     const list = document.querySelector(".techList");
     removeAllChildNodes(list);
+
+    // removing added footer buttons
+    const footer = document.querySelector(".modal-footer")
+    removeAllChildNodes(footer);
+
+    // removing added technology used header
+    const techHeader = document.querySelector(".techHeader")
+    techHeader.remove();
 });
